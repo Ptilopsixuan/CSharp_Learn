59895df1f8112259202084d33bc5c8f1ba759a21
Ptilopsixu_n <154034199+Ptilopsixuan@users.noreply.github.com>
2024-04-02 10:20:22 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+this folder is solution folder, which can contain multy projects.
+this solution floder is created for logging my learning of C#.
---
+++ first/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first//命名空间
+{
+    internal class Program//类
+    {
+        //必须包含Main函数，执行的入口点。
+        static void Main(string[] args)//函数，方法
+        {
+            try { }
+            catch { }
+        }
+    }
+
+
+}
